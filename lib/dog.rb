45ddcb998be4541bf3e0f347806c4a3bda782f3e
Russell Sutter <russell.sutter@gmail.com>
2020-08-04 14:42:01 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,15 @@
 class Dog
-    def initialize(name)
-        @name = name
-    end
     def name
         @name
     end
     def name=(name)
         @name = name
+    end
+    def breed
+        @breed
+    end
+
+    def breed=(breed)
+        @breed = breed
     end
 end
